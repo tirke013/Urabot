a91838de253e226e1eccaf99bf3a6c27852d18cd
tirke013 <81182448+tirke013@users.noreply.github.com>
2021-04-16 13:17:16 +0200

    variable name change

--- jsonParse.py
+++ jsonParse.py
@@ -4,7 +4,7 @@ def parse(name, path):
     with open(path+"\message_1.json", "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    pisanje = open(path+"\parsed.txt", "a+", encoding="utf-8")
+    writing = open(path+"\parsed.txt", "a+", encoding="utf-8")
 
     newdict = data["messages"]
 
@@ -12,8 +12,8 @@ def parse(name, path):
         if line["sender_name"].encode("latin-1").decode("unicode-escape") == name.encode("utf-8").decode("unicode-escape") and line["type"] == "Generic" and "content" in line:
             if line["content"] != "Liked a message":
                 print(line["content"])
-                pisanje.write(line["content"]+"\n")
-    pisanje.close()
+                writing.write(line["content"]+"\n")
+    writing.close()
     readLower = open(path+"\parsed.txt", "r", encoding="utf-8")
     text = readLower.read()
     writeLower = open(path+"\parsed.txt", "w", encoding="utf-8")
